cdf1d712860006d8638e0a895e3b9054d6dbb463
manish-giri <manish.giri.me@gmail.com>
2020-02-23 23:03:13 -0500

    Add runtime

--- Algorithms/java/src/deletenodeinlinkedlist/Solution.java
+++ Algorithms/java/src/deletenodeinlinkedlist/Solution.java
@@ -52,3 +52,11 @@ class Solution {
         node.next = node.next.next;
     }
 }
+
+// Time - O(1)
+/*
+Success
+Details
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Delete Node in a Linked List.
+Memory Usage: 40.4 MB, less than 5.09% of Java online submissions for Delete Node in a Linked List.
+ */
