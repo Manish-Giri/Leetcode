79d80ec56ece62338c8f92e89485b030cc19d136
MANISH-GIRI <manish.giri.me@gmail.com>
2020-02-26 01:53:51 -0500

    Solve Happy Number in Python3

---
+++ Algorithms/java/src/binarytreepostordertraversal/RecursiveSolution.java
@@ -0,0 +1,39 @@
+package binarytreepostordertraversal;
+
+import datastructures.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given a binary tree, return the postorder traversal of its nodes' values.
+ *
+ * Example:
+ *
+ * Input: [1,null,2,3]
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ *
+ * Output: [3,2,1]
+ */
+
+public class RecursiveSolution {
+    List<Integer> al = new ArrayList<>();
+    public List<Integer> postorderTraversal(TreeNode root) {
+        helper(root);
+        return al;
+    }
+    private void helper(TreeNode root) {
+        if(root == null)
+            return;
+        helper(root.left);
+        helper(root.right);
+        al.add(root.val);
+    }
+
+
+
+}
