c7064b9ddbac84143c4634af08946d8ecdf69281
manish-giri <manish.giri.me@gmail.com>
2020-04-05 04:57:21 -0400

    Add 2nd solution to Move Zeroes #283

--- Algorithms/java/src/movezeroes/Solution.java
+++ Algorithms/java/src/movezeroes/Solution.java
@@ -14,7 +14,15 @@ package movezeroes;
  */
 
 class Solution {
-    void moveZeroes(int[] nums) {
+
+    /*
+    Details
+    Runtime: 3 ms, faster than 14.27% of Java online submissions for Move Zeroes.
+    Memory Usage: 40.3 MB, less than 5.59% of Java online submissions for Move Zeroes.
+
+    Brute Force solution - Time O(n^2)
+     */
+    void moveZeroes_ApproachZero(int[] nums) {
         int i = 0, j = -1;
         while (i < nums.length - 1) {
             if (nums[i] != 0) {
@@ -36,10 +44,29 @@ class Solution {
             i++;
         }
     }
+
+
+    // Approach 2 - Optimal space, sub optimal # of operations
+    /* Details
+    Runtime: 0 ms, faster than 100.00% of Java online submissions for Move Zeroes.
+    Memory Usage: 40.1 MB, less than 5.59% of Java online submissions for Move Zeroes.
+
+    Time - O(n) with sub-optimal number of operations, because too many writes of zeroes in 2nd iteration
+    for arrays which are mostly zeroes - [0,0,0,0,0,1]
+    Space - O(1)
+     */
+    void moveZeroes_ApproachTwo(int[] nums) {
+        int placeNextNonZeroElementAt = 0;
+        // 1st iteration - copy all non zeroes, overwrite
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[placeNextNonZeroElementAt++] = nums[i];
+            }
+        }
+        // 2nd iteration - set remaining zeroes
+        for (int i = placeNextNonZeroElementAt; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
 }
 
-/*
-Details
-Runtime: 3 ms, faster than 14.27% of Java online submissions for Move Zeroes.
-Memory Usage: 40.3 MB, less than 5.59% of Java online submissions for Move Zeroes.
- */
