df2db1c1275a19cabf0a7aa9497d6d6d910578c4
manish-giri <manish.giri.me@gmail.com>
2021-04-05 23:44:00 -0400

    Update solution majority element

--- Algorithms/java/src/majorityelement/bruteforce/Solution.java
+++ Algorithms/java/src/majorityelement/bruteforce/Solution.java
@@ -6,22 +6,22 @@ import java.util.HashMap;
 
  */
 
- class Solution {
-    
+class Solution {
+
     /**
      * Runtime: 13 ms, faster than 11.44% of Java online submissions for Majority Element.
      * Memory Usage: 53.6 MB, less than 6.30% of Java online submissions for Majority Element.
      * Time - O(n) | Space - O(n)
-    */
-    
-     int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> count = new HashMap<>();
-        for(int n: nums) {
+     */
+
+    int majorityElement(int[] nums) {
+        var count = new HashMap<Integer, Integer>();
+        for (int n : nums) {
             count.put(n, count.getOrDefault(n, 0) + 1);
         }
-        int majority = nums.length/2;
-        for(var pair: count.entrySet()) {
-            if(pair.getValue() > majority)
+        int majority = nums.length / 2;
+        for (var pair : count.entrySet()) {
+            if (pair.getValue() > majority)
                 return pair.getKey();
         }
         return -1;
