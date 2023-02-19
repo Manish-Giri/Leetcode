9f58ff9604fe01e065c3c4600ea524c844aeb682
manish-giri <manish.giri.me@gmail.com>
2023-02-18 19:36:34 -0600

    Solve Roman To Integer

---
+++ Algorithms/java/src/HASHTABLES/romantointeger/attempt/Solution.java
@@ -0,0 +1,93 @@
+package HASHTABLES.romantointeger.attempt;
+
+import java.util.HashMap;
+
+/**
+ * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+ * <p>
+ * Symbol       Value
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ * For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+ * <p>
+ * Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+ * <p>
+ * I can be placed before V (5) and X (10) to make 4 and 9.
+ * X can be placed before L (50) and C (100) to make 40 and 90.
+ * C can be placed before D (500) and M (1000) to make 400 and 900.
+ * Given a roman numeral, convert it to an integer.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: s = "III"
+ * Output: 3
+ * Explanation: III = 3.
+ * Example 2:
+ * <p>
+ * Input: s = "LVIII"
+ * Output: 58
+ * Explanation: L = 50, V= 5, III = 3.
+ * Example 3:
+ * <p>
+ * Input: s = "MCMXCIV"
+ * Output: 1994
+ * Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= s.length <= 15
+ * s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+ * It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+ */
+class Solution {
+    int romanToInt(String s) {
+        HashMap<String, Integer> hm = new HashMap<>();
+        hm.put("I", 1);
+        hm.put("V", 5);
+        hm.put("X", 10);
+        hm.put("L", 50);
+        hm.put("C", 100);
+        hm.put("D", 500);
+        hm.put("M", 1000);
+
+        int total = 0;
+        int i = 0;
+        while (i < s.length()) {
+            // case 1 - 2 characters available for comparison
+            if (i + 1 < s.length()) {
+
+                // case 1.a - left >= right
+                String curr = String.valueOf(s.charAt(i));
+                String next = String.valueOf(s.charAt(i + 1));
+                if (hm.get(curr) >= hm.get(next)) {
+                    total += hm.get(curr);
+                    // move one step
+                    i++;
+                }
+
+                // case 1.b - left < right
+                else {
+                    total += hm.get(next) - hm.get(curr);
+                    // move two steps
+                    i += 2;
+                }
+            }
+
+            // case 2 - only 1 character available
+            else {
+                String curr = String.valueOf(s.charAt(i));
+                total += hm.get(curr);
+                i++;
+            }
+        }
+        return total;
+    }
+}
