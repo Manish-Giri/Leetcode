99680fc0d34e67a74e0a5e808bd8236c2d8d5dff
Manish Giri <manish.giri.me@gmail.com>
2023-11-04 21:38:27 -0500

    Add Go solution for Single Element in Sorted Array

--- Algorithms/go/arrays/main.go
+++ Algorithms/go/arrays/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"arrays/singleelementinsortedarray"
 	"arrays/twosum"
 	"fmt"
 )
@@ -10,5 +11,9 @@ func main() {
 	target := 9
 	result := twosum.TwoSum(nums, target)
 	fmt.Println("Result: ", result)
+
+	nums1 := []int{1, 1, 2, 3, 3, 4, 4, 8, 8}
+	result1 := singleelementinsortedarray.SingleNonDuplicate(nums1)
+	fmt.Println("Result of Single Element in Sorted Array: ", result1)
 
 }
---
+++ Algorithms/go/arrays/singleelementinsortedarray/singleelementattempt.go
@@ -0,0 +1,10 @@
+package singleelementinsortedarray
+
+func SingleNonDuplicate(nums []int) int {
+	num := nums[0]
+
+	for i := 1; i < len(nums); i++ {
+		num = num ^ nums[i]
+	}
+	return num
+}
